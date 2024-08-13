80c2a13a8183bb8b6a4b360acb3fc34b3676592d
Balajee Choudula <baluworks@gmail.com>
2024-08-13 16:47:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   build: {
     outDir: 'build'
-  }
+  },
   plugins: [react()],
+  base: '/react-redux-vite-app/'
   server: {
     port: 3000,
   },
