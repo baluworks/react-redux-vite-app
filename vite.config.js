1ca8c897eea987719a458710e1aa2298f8ba85af
baluworks <baluworks@gmail.com>
2024-11-02 15:32:36 +0530

    vite config changes rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: './src/main.jsx',
       output: {
-        entryFileNames: 'assets/[name].js',
+        entryFileNames: '[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
       }
