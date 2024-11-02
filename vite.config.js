55060f938661d38e272baac78a0f30e93ec006a6
baluworks <baluworks@gmail.com>
2024-11-02 12:10:25 +0530

    vite config changes rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      input: './src/main.jsx',
+      output: {
+        entryFileNames: 'main.js'
+      }
+    }
   },
   plugins: [react()],
   base: '/react-redux-vite-app/',
